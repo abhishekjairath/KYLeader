8c691b9020f4cbbf3a28523b77d68c786bb44942
jairath.abhishek <jairath.abhishek@gmail.com>
2013-11-11 00:50:53 +0530

    Lots of updates
    * Login
    * Overall Layout
    * States to constituency

--- README.md
+++
@@ -1,8 +0,0 @@
-KYLeader
-========
-
-Semester-3 Project @ JIIT
-
-This repository is based on my college project and it's soul purpose is to save data to a secure place in order to avoid any losses and have easy backup.
-
-The basic function of this project is to act as a source of information, for voters about the Lok Sabha Elections(India). The user can also rate the candidates contesting the elections which will give a general idea about the election results.
--- app.js
+++ app.js
@@ -1,99 +1,147 @@
 var application_root = __dirname,
     express = require("express"),
-	mysql = require('mysql');
-    path = require("path");
-var passport = require('passport')
- LocalStrategy = require('passport-local').Strategy;
+    mysql = require('mysql'),
+    path = require("path"),
+    nib = require("nib"),
+    passport = require('passport'),
+    stylus = require('stylus'),
+    LocalStrategy = require('passport-local').Strategy;
 
 var app = express();
 
 // Database
-
 var connection = mysql.createConnection({
 host : 'localhost',
 user : 'root',
 password : '',
-database: "user"
+database: "user",
+multipleStatements : true
 });
 
+//NIB and STYLUS
+function compile(str, path) {
+  return stylus(str)
+    .set('filename', path)
+    .use(nib())
+}
 // Config
-
-app.configure(function () {
+  app.configure(function () {
   app.use(express.bodyParser());
   app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(path.join(application_root, "/")));
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
-/*
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-    User.findOne({ username: username }, function(err, user) {
-      if (err) { return done(err); }
-      if (!user) {
-        return done(null, false, { message: 'Incorrect username.' });
-      }
-      if (!user.validPassword(password)) {
-        return done(null, false, { message: 'Incorrect password.' });
-      }
-      return done(null, user);
-    });
+  app.use(express.cookieParser("secret"));
+  app.use(express.session({
+    secret: 'keyboard cat'
+  }));
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(stylus.middleware(
+  { src: __dirname + '/public'
+  , compile: compile
   }
-));
-*/
+  ));
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(express.static(__dirname + '/public'))
+  app.use(app.router);
+});
+
+
+passport.use(new LocalStrategy(
+ 
+    function(username, password, done) {
+ 
+        return check_auth_user(username,password,done);
+ 
+    }
+ 
+    ));
+
+
 //Register
+app.get('/registerview', function (req, res) {
+  res.render('register.jade',
+  { title : 'Register' }
+  );
+});
 app.post('/insertuser', function (req, res){
   console.log("POST: ");
   username = req.body.user;
-  password=req.body.user;
-  fullname = req.body.user;
-  email = req.body.user;
-  sex = req.body.user;
-  city = req.body.user;
+  password=req.body.password;
+  fullname = req.body.fullname;
+  email = req.body.email;
+  sex = req.body.sex;
+  city = req.body.city;
   connection.query('insert into users ( username ,password,fullname , email , sex , city ) values (' + '"' + username +'"' +',' + '"'+password + '"' + ',' + '"' + fullname +'"'+','+'"'+email+'"' + ',' + '"' + sex +'"' +','  + '"' + city +'"' +');', function (error, rows, fields) { 
-		//console.log(error);
-         res.writeHead(200, {'Content-Type': 'text/plain'});
+		        res.writeHead(200, {'Content-Type': 'text/plain'});
             res.end( 'record inserted...');
 		      }); 
 });
 
-/*/Login
-app.post('/login',passport.authenticate('local', { 
-	                               successRedirect: '/',
-                                   failureRedirect: '/login',
-                                   failureFlash: true })
+//Login
+app.get('/loginview', function (req, res) {
+  res.render('login.jade',
+  { title : 'Login' }
+  );
+});
+app.post('/login',
+  passport.authenticate('local', { successRedirect: '/',
+                                   failureRedirect: '/loginview'
+                                 })
 );
-*/
-//Constituency Information
-app.get('/state/:sid', function (req, res) {
-         
-         connection.query('CALL `states` ('+req.params.sid+')'+';', function (error, rows, fields) { 
-         res.writeHead(200, {'Content-Type': 'text/plain'});
-		 str='';
-		 if(rows==null)
-		 	console.log('No record found');
-		 else{
-		 for(i=0;i<rows.length;i++)
-			str = str + rows[i].name + rows[i].current_mp + rows[i].current_party +'\n';
-		 res.end( str);
-		}
-      }); 
+
+app.get('/logout', function (req, res){
+  req.logout();
+  res.redirect('/');
 });
 
-app.get('/state1', function (req, res) {
-         
-         connection.query('CALL `states` (1);', function (error, rows, fields) { 
-         res.writeHead(200, {'Content-Type': 'text/plain'});
-     str='';
-     if(rows==null)
-      console.log('No record found');
-     else{
-     for(i=0;i<rows.length;i++)
-      str = str + rows[i].name + rows[i].current_mp + rows[i].current_party +'\n';
-     res.end( str);
-    }
-      }); 
+function check_auth_user(username,password,done){
+    var sql='SELECT * FROM users WHERE username ='+ '"'+ username +'"'+' and password = '+'"'+ password+'";'; 
+    connection.query(sql,
+        function (err,results,fields) {
+ 
+            if (err) throw err;
+ 
+            if(results.length > 0){
+ 
+                var res=results[0]; 
+                //serialize the query result save whole data as session in req.user[] array  
+                passport.serializeUser(function(res, done) {
+                    done(null,res);
+                });
+ 
+                passport.deserializeUser(function(id, done) {
+                    done(null,res);
+ 
+                });
+                //console.log(JSON.stringify(results));
+                //console.log(results[0]['member_id']);
+                return done(null, res);
+            }
+            else
+            {
+              return done(null, false); 
+            }
+ 
+        });
+ 
+}
+
+
+//Constituency Information
+app.get('/state/:sid', function (req, res) {
+  connection.query('select name,current_mp,current_party from constituency where constituency.sid='+req.params.sid+';', function (err,rows,field) {
+        res.render('const.jade',
+         {
+          num : '1',
+          name : rows[0].name,
+          crmp : rows[0].current_mp,
+          crpr : rows[0].current_party
+         }
+        );  
+    });
 });
+
 
 app.get('/users/:username', function (req, res){
 	connection.query('SELECT * FROM users where username ='+'"'+req.params.username+'"'+';', function (error, rows, fields) { 
@@ -114,6 +162,12 @@ app.get('/users/:username', function (req, res){
 });
 
 // Launch server
-
+app.get('/', function (req, res) {
+  res.render('index',
+  { title : 'Home',
+    scripts: ['public/js/main.js']
+   }
+  )
+})
 app.listen(1212);
 console.log('Server running at http://127.0.0.1:1212');
